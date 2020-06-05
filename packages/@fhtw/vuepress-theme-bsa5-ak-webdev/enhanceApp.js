aaed7a7059780b36ebef347e4d52268e5e4bb160
Alija Sabic <sabic.alija@gmail.com>
2020-06-05 09:49:32 +0200

    Remove previous components and dependencies

--- packages/@fhtw/vuepress-theme-bsa5-ak-webdev/enhanceApp.js
+++ packages/@fhtw/vuepress-theme-bsa5-ak-webdev/enhanceApp.js
@@ -1,9 +1,10 @@
-import "./styles/main.scss";
-import "./plugins/bootstrap";
-
+// async function is also supported, too
 export default ({
   Vue, // the version of Vue being used in the VuePress app
   options, // the options for the root Vue instance
   router, // the router instance for the app
-  siteData // site metadata
-}) => {};
+  siteData, // site metadata
+  isServer, // is this enhancement applied in server-rendering or client
+}) => {
+  // ...apply enhancements to the app
+};
--- packages/@fhtw/vuepress-theme-bsa5-ak-webdev/index.js
+++ packages/@fhtw/vuepress-theme-bsa5-ak-webdev/index.js
@@ -1,28 +1,6 @@
-const twemoji = require("twemoji");
 module.exports = (themeConfig, ctx) => {
   const defaultDirectoryClassifierPluginOptions = {
-    directories: [
-      {
-        id: "projects",
-        dirname: "projects",
-        layout: "IndexProjects",
-        itemLayout: "Project",
-        nav: {
-          title: "Projects",
-          order: "append"
-        }
-      },
-      {
-        id: "references",
-        dirname: "references",
-        layout: "IndexReferences",
-        itemLayout: "Reference",
-        nav: {
-          title: "References",
-          order: "append"
-        }
-      }
-    ]
+    directories: [],
   };
 
   const { modifyDirectoryClassifierOptions } = themeConfig;
@@ -33,30 +11,13 @@ module.exports = (themeConfig, ctx) => {
       : defaultDirectoryClassifierPluginOptions;
 
   const plugins = [["@fhtw/directory-classifier", directoryClassifierPluginOptions]];
-  const extendMarkdown = md => {
+  const extendMarkdown = (md) => {
     // md.set({ html: true, breaks: true, typographer: true, linkify: true });
-    md.use(require("markdown-it-sup"));
-    md.use(require("markdown-it-sub"));
-    md.use(require("markdown-it-abbr"));
-    md.use(require("markdown-it-deflist"));
-    md.use(require("markdown-it-footnote"));
-    md.use(require("markdown-it-ins"));
-    md.use(require("markdown-it-mark"));
-    md.use(require("markdown-it-checkbox"));
-    md.use(require("markdown-it-kbd"));
-    md.use(require("markdown-it-imsize"));
-    md.renderer.rules.emoji = function(token, idx) {
-      return twemoji.parse(token[idx].content, {
-        base: "https://twemoji.maxcdn.com/2/",
-        ext: ".svg",
-        folder: "svg"
-      });
-    };
   };
   const config = {
     extend: "@vuepress/theme-default",
     plugins,
-    extendMarkdown
+    extendMarkdown,
   };
 
   return config;
