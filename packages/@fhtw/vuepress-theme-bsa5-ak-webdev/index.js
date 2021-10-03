871be07e739cc346586fc3ce2dbc102455417b43
Alija Sabic <sabic.alija@gmail.com>
2021-10-03 15:36:52 +0200

    Update @fhtw/vuepress-theme-bsa5-ak-webdev: add markdown-it plugins

--- packages/@fhtw/vuepress-theme-bsa5-ak-webdev/index.js
+++ packages/@fhtw/vuepress-theme-bsa5-ak-webdev/index.js
@@ -5,6 +5,8 @@ module.exports = (themeConfig, ctx) => {
     md.use(require("markdown-it-abbr"));
     md.use(require("markdown-it-sup"));
     md.use(require("markdown-it-sub"));
+    md.use(require("markdown-it-multimd-table"), { rowspan: true });
+    md.use(require("markdown-it-attrs"));
   };
   const config = {
     extend: "@vuepress/theme-default",
